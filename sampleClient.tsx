aa37e2177e9137e6aa412e87319af967d85e8e0c
Chebotarev-Alexey <66257265+Chebotarev-Alexey@users.noreply.github.com>
2025-06-18 00:11:14 +0300

    update sample code

--- sampleClient.tsx
+++ sampleClient.tsx
@@ -1,37 +1,37 @@
-'use client';
+import React from 'react';
 
-import { useState } from 'react';
-import type { Exposed } from './sampleServer';
+declare const $state: any;
+declare const $derive: any;
 
-declare const useExposed: <TExposed extends { read: object; write: object }>(
-  apiUrl: string
-) => TExposed['read'] & TExposed['write'];
+const $client = (user) => {
+  const [value, setValue] = $state('');
 
-const App = (apiUrl: string) => {
-  const { email, notes } = useExposed<Exposed>(apiUrl);
-  const [value, setValue] = useState<string>();
-
-  return (
-    <>
-      <h1>Notes app | Your email is {email}</h1>
-      <ul>
-        {notes.map((note, key) => (
-          <li id={key}>
-            {note.content} | {note.timeCreated}
-          </li>
-        ))}
-      </ul>
-      <input onChange={(e) => setValue(e.target.value)}>{value}</input>
-      <button
-        onClick={() => {
-          notes.push(value).then(() => {
-            console.log('Note added');
-          });
-          setValue('');
-        }}
-      >
-        Add note
-      </button>
-    </>
+  return $derive(
+    (email, notes) => (
+      <>
+        <h1>Notes app | Your email is {email}</h1>
+        <ul>
+          {notes.map((note, key) => (
+            <li id={key}>
+              {note.content} | {note.timeCreated}
+            </li>
+          ))}
+        </ul>
+        <input onChange={(e) => setValue(e.target.value)}>{value}</input>
+        <button
+          onClick={() => {
+            notes.push(value).then(() => {
+              console.log('Note added');
+            });
+            setValue('');
+          }}
+        >
+          Add note
+        </button>
+      </>
+    ),
+    [user.email, user.notes]
   );
 };
+
+export default $client;
--- sampleServer.ts
+++ sampleServer.ts
@@ -1,117 +1,81 @@
 'use server';
 
-import express from 'express';
-import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import axios from 'axios';
 
-declare const useTRPC: any;
-declare const usePrisma: any;
-declare const useEffect: any;
-declare const useConst: any;
-declare const useDerived: any;
+declare const $drizzle: any;
+declare const $many: any;
 
-declare const createContextJWT: any;
+declare const $hostClient: any;
 
-declare const semanticStatisticsSchema: any;
+const $note = (drizzle, getNotes) => {
+  const { content } = drizzle.$state('content')(drizzle.string().max(1024));
 
-const useNote = (prisma, getNotes) => {
-  const { content } = prisma.useState('content', prisma.string().max(1024));
   return {
     content,
-    ...prisma.useState('references', prisma.linkElement(prisma.lazy(getNotes))),
-    ...prisma.useState('secretField', prisma.string()),
-    ...prisma.useState('timeCreated', prisma.string()),
-    ...prisma.useEffectState(
-      'semanticStatistics',
-      semanticStatisticsSchema,
-      async () => {
-        const res = await axios.get('example-semantics-api.com/', {
-          params: { content: content.get() }
+    ...drizzle.$state('references')(
+      drizzle.linkElement(drizzle.lazy(getNotes))
+    ),
+    ...drizzle.$state('secretField')(drizzle.string()),
+    ...drizzle.$effect('wordCount')(
+      drizzle.number(),
+      async (content) => {
+        const res = await axios.get('https://count-words.com/', {
+          params: { content }
         });
         return res.data;
-      }
-    )
-  };
-};
-
-const usePublicNote = (note) => {
-  return {
-    content: note.content,
-    references: note.references.forEach(usePublicNote).const,
-    timeCreated: note.timeCreated.const
-  };
-};
-
-const useNotes = (name, prisma) => {
-  const obj = prisma.useMany(name, () => useNote(prisma, () => obj[name]));
-  return obj;
-};
-
-const useUser = (prisma) => {
-  const { notes } = useNotes('notes', prisma);
-  const email = prisma.useState('email', prisma.string().email());
-  const hashedPassword = prisma.useState('hashedPassword', prisma.string());
-  return { notes, email, hashedPassword };
-};
-
-const useUsers = (name, prisma) => {
-  return { ...prisma.useMany(name, () => useUser(prisma)) };
-};
-
-const useTRPCExpress = (trpcRouter) => {
-  const app = useConst(() => {
-    return express();
-  });
-
-  const expressMiddleware = useDerived(
-    () =>
-      createExpressMiddleware({
-        router: trpcRouter.get(),
-        createContext: createContextJWT
-      }),
-    [trpcRouter]
-  );
-
-  useEffect(() => {
-    app.use('/trpc', expressMiddleware.get());
-  }, [app]);
-
-  return app;
-};
-
-const useExposeToClient = <AuthCtx extends object, Exposed extends unknown>(
-  expose: (authCtx: AuthCtx) => Exposed,
-  serverPort: number
-) => {
-  const trpcRouter = useTRPC(expose);
-  const app = useTRPCExpress(trpcRouter);
-  useEffect(() => {
-    app.listen(serverPort, () => {
-      console.log(`Server running on http://localhost:${serverPort}/trpc`);
-    });
-  }, [app]);
-
-  return { exposedType: {} as Exposed };
-};
-
-export const useServer = (serverPort: number) => {
-  const { prisma } = usePrisma();
-
-  const { users } = useUsers('users', prisma);
-
-  return useExposeToClient(({ userKey }: { userKey: typeof users.key }) => {
-    const user = users.getByKey(userKey);
-    const notes = user.notes.forEach(usePublicNote);
-    return {
-      read: {
-        email: user.email,
-        notes: notes
       },
-      write: {
-        notes: notes
-      }
-    };
-  }, serverPort);
+      [content]
+    ),
+    ...drizzle.$derived('includesLetterB')(
+      drizzle.boolean(),
+      (content) => content.includes('b'),
+      [content]
+    ),
+    ...drizzle.$state('timeCreated')(drizzle.date(), {
+      initial: () => new Date()
+    })
+  };
 };
 
-export type Exposed = ReturnType<typeof useServer>['exposedType'];
+const $publicNotes = (notes) => {
+  return notes.map(
+    (note) => ({
+      content: note.content.const,
+      references: $publicNotes(note.references),
+      timeCreated: note.timeCreated.const,
+      wordCount: note.wordCount,
+      includesLetterB: note.includesLetterB
+    }),
+    [notes]
+  );
+};
+
+const $user = (drizzle) => {
+  const notes = $many(() => drizzle.row('note')($note(drizzle, () => notes)));
+  return {
+    notes,
+    ...drizzle.$state('email')(drizzle.string()),
+    ...drizzle.$state('hashedPassword')(drizzle.string())
+  };
+};
+
+const $publicUsers = (users) => {
+  return users.map((user) => ({
+    notes: $publicNotes(user.notes),
+    email: user.email
+  }));
+};
+
+export const $app = () => {
+  const { drizzle } = $drizzle();
+
+  const { users } = $many(() => drizzle.row('user')($user(drizzle)));
+
+  $many('users', drizzle);
+
+  $hostClient(
+    './sampleClient.tsx',
+    () => import('./sampleClient.tsx'),
+    $publicUsers(users)
+  );
+};
